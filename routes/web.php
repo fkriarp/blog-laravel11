e1082ed9ab3cec04870d87521a4055433e5e3ca9
Fikri <fikri.akbar1p@gmail.com>
2024-12-07 22:48:41 +0700

    memindahkan array  kedalam Class

--- routes/web.php
+++ routes/web.php
@@ -3,48 +3,40 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
+class Posts 
+{
+    public static function all() 
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' => 'Judul Artikel 1',
+                'author' => 'Fikri Akbar Pratama',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, facere. Neque incidunt nisi esse expedita. Amet eveniet quia ipsum, earum nesciunt iure, labore ea mollitia atque quasi quidem? Explicabo at hic sunt pariatur nihil atque doloremque sint numquam ab voluptas aliquam dolores quae dolorum, illo corporis ipsum minus architecto iure?',
+            ],
+            [
+                'id' => 2,
+                'slug' => 'judul-artikel-2',
+                'title' => 'Judul Artikel 2',
+                'author' => 'Fikri Akbar Pratama',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id facere unde natus culpa. Maiores corporis inventore aspernatur neque vel, hic nostrum sapiente tempora incidunt iusto id corrupti laudantium itaque sit eligendi. Eum asperiores maxime nobis repudiandae, nesciunt voluptas quisquam placeat.'
+            ]
+        ];
+    }
+}
+
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 })->name('home');
 
 Route::get('/posts', function () {  
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Fikri Akbar Pratama',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, facere. Neque incidunt nisi esse expedita. Amet eveniet quia ipsum, earum nesciunt iure, labore ea mollitia atque quasi quidem? Explicabo at hic sunt pariatur nihil atque doloremque sint numquam ab voluptas aliquam dolores quae dolorum, illo corporis ipsum minus architecto iure?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Fikri Akbar Pratama',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id facere unde natus culpa. Maiores corporis inventore aspernatur neque vel, hic nostrum sapiente tempora incidunt iusto id corrupti laudantium itaque sit eligendi. Eum asperiores maxime nobis repudiandae, nesciunt voluptas quisquam placeat.'
-        ]
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Posts::all()]);
 })->name('posts');
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Fikri Akbar Pratama',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, facere. Neque incidunt nisi esse expedita. Amet eveniet quia ipsum, earum nesciunt iure, labore ea mollitia atque quasi quidem? Explicabo at hic sunt pariatur nihil atque doloremque sint numquam ab voluptas aliquam dolores quae dolorum, illo corporis ipsum minus architecto iure?',
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Fikri Akbar Pratama',
-            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id facere unde natus culpa. Maiores corporis inventore aspernatur neque vel, hic nostrum sapiente tempora incidunt iusto id corrupti laudantium itaque sit eligendi. Eum asperiores maxime nobis repudiandae, nesciunt voluptas quisquam placeat.'
-        ]
-    ];
 
-    $post = Arr::first($posts, function($post) use ($slug) {
+    $post = Arr::first(Posts::all(), function($post) use ($slug) {
         return $post['slug'] == $slug;
     });
 
