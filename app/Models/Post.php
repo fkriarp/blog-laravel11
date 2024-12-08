bd6245a45387f902688124258724f1678d5821aa
Fikri <fikri.akbar1p@gmail.com>
2024-12-08 14:12:21 +0700

    Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,43 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all() 
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Fikri Akbar Pratama',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet, facere. Neque incidunt nisi esse expedita. Amet eveniet quia ipsum, earum nesciunt iure, labore ea mollitia atque quasi quidem? Explicabo at hic sunt pariatur nihil atque doloremque sint numquam ab voluptas aliquam dolores quae dolorum, illo corporis ipsum minus architecto iure?',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Fikri Akbar Pratama',
-                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id facere unde natus culpa. Maiores corporis inventore aspernatur neque vel, hic nostrum sapiente tempora incidunt iusto id corrupti laudantium itaque sit eligendi. Eum asperiores maxime nobis repudiandae, nesciunt voluptas quisquam placeat.'
-            ]
-        ];
-    }
-
-    public static function find($slug): Array 
-    {
-        // return Arr::first(static::all(), function($post) use ($slug) {
-        //     return $post['slug'] == $slug;
-        // });
-
-        // menggunakan arrow function | lebih singkat
-        $post = Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        // apabila slug yang dicari tidak ditemukan, maka akan menghasilkan halaman error 404
-        if (! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+   protected $fillable = [
+    'title',
+    'author',
+    'slug',
+    'body'
+   ];
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,10 +5,9 @@
     <article class="py-8 max-w-screen-md border-b border-gray-300">
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
         <div class="text-base text-gray-500">
-            <a href="#">{{ $post['author'] }}</a> | 7 Desember 2024 
+            <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumans() }} 
         </div>
         <p class="my-4 font-light">{{ $post['body'], 150 }}</p>
         <a href="{{ route('posts') }}" class="font-medium text-blue-500 hover:underline">&laquo; Back to post</a>
     </article>
-    
 </x-layout>
